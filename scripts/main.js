03160feedc0191572aa3223798f313500e14dd81
Adam Eslinger <adameslinger@hotmail.com>
2021-09-20 23:38:17 -0500

    Adds learn-more page. Stubs in other pages

--- scripts/main.js
+++ scripts/main.js
@@ -5,9 +5,9 @@ if (
 ) {
     let observer = new IntersectionObserver(entries => {
         if (entries[0].boundingClientRect.y < 0) {
-            document.body.classList.add("header-not-at-top");
+            document.body.classList.add("nav-not-at-top");
         } else {
-            document.body.classList.remove("header-not-at-top");
+            document.body.classList.remove("nav-not-at-top");
         }
     });
     observer.observe(document.querySelector("#top-of-site-pixel-anchor"));
